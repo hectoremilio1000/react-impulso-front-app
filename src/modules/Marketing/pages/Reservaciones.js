d00e1ba31cdb9fff652111fcaee76701e68edb4e
jampierzcode <jampierv127@gmail.com>
2024-12-27 17:12:04 -0500

    reservacioniones inicio ruteo api

--- src/modules/Marketing/pages/Reservaciones.js
+++ src/modules/Marketing/pages/Reservaciones.js
@@ -1,101 +1,149 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Table, Tag } from "antd";
+import axios from "axios";
+import { useAuth } from "../../../components/AuthContext";
 
 const Reservaciones = () => {
-  // Dummy data para la tabla
-  const data = [
-    {
-      key: "1",
-      calendario: "Calendario Principal",
-      nombre: "Juan Pérez",
-      email: "juan.perez@example.com",
-      fecha: "18 de junio 2024",
-      cantidad: 4,
-      estado: "Confirmada",
-    },
-    {
-      key: "2",
-      calendario: "Calendario Principal",
-      nombre: "María López",
-      email: "maria.lopez@example.com",
-      fecha: "19 de junio 2024",
-      cantidad: 2,
-      estado: "Pendiente",
-    },
-    {
-      key: "3",
-      calendario: "Calendario VIP",
-      nombre: "Carlos García",
-      email: "carlos.garcia@example.com",
-      fecha: "20 de junio 2024",
-      cantidad: 5,
-      estado: "Cancelada",
-    },
-    // Agrega 17 registros más...
-    ...Array.from({ length: 17 }, (_, index) => ({
-      key: index + 4,
-      calendario: index % 2 === 0 ? "Calendario VIP" : "Calendario Principal",
-      nombre: `Persona ${index + 1}`,
-      email: `persona${index + 1}@example.com`,
-      fecha: `${index + 10} de julio 2024`,
-      cantidad: Math.floor(Math.random() * 6) + 1,
-      estado: ["Confirmada", "Pendiente", "Cancelada"][
-        Math.floor(Math.random() * 3)
-      ],
-    })),
-  ];
+  const { auth } = useAuth();
+  const [listEventTypes, setListEventTypes] = useState([]);
 
-  // Columnas de la tabla
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  const [isAuthenticatedWithCalendly, setIsAuthenticatedWithCalendly] =
+    useState(false);
+
+  useEffect(() => {
+    const checkCalendlyAuth = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/calendly/status`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${auth.token}`,
+          },
+        });
+
+        if (response.data.isAuthenticated) {
+          setIsAuthenticatedWithCalendly(true);
+        } else {
+          setIsAuthenticatedWithCalendly(false);
+        }
+      } catch (error) {
+        console.error(
+          "Error verificando la autenticación con Calendly:",
+          error
+        );
+        setIsAuthenticatedWithCalendly(false);
+      }
+    };
+
+    checkCalendlyAuth();
+  }, [auth.token]);
+  useEffect(() => {
+    if (isAuthenticatedWithCalendly) {
+      const getEventTypes = async () => {
+        try {
+          const response = await axios.get(`${apiUrl}/calendly/event_types`, {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${auth.token}`,
+            },
+          });
+          console.log(response);
+          const newData =
+            response.data.data.length > 0 ? response.data.data : [];
+          setListEventTypes(newData);
+        } catch (error) {
+          console.error("Error al traer los tipo de eventos:", error);
+        }
+      };
+
+      getEventTypes();
+    }
+  }, [isAuthenticatedWithCalendly]);
+
   const columns = [
     {
-      title: "Calendario",
-      dataIndex: "calendario",
-      key: "calendario",
+      title: "Event Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => (
+        <span className="text-purple-600 font-bold">{text}</span>
+      ),
     },
     {
-      title: "Nombre",
-      dataIndex: "nombre",
-      key: "nombre",
+      title: "Duration (mins)",
+      dataIndex: "duration",
+      key: "duration",
     },
     {
-      title: "Email",
-      dataIndex: "email",
-      key: "email",
+      title: "Description",
+      dataIndex: "description_plain",
+      key: "description",
+      ellipsis: true,
+      render: (text) => <span className="text-sm text-gray-700">{text}</span>,
     },
     {
-      title: "Fecha",
-      dataIndex: "fecha",
-      key: "fecha",
+      title: "Location",
+      dataIndex: "locations",
+      key: "locations",
+      render: (locations) =>
+        locations.map((loc, index) => (
+          <div key={index} className="text-sm text-gray-600">
+            {loc.location}
+          </div>
+        )),
     },
     {
-      title: "Cantidad de Personas",
-      dataIndex: "cantidad",
-      key: "cantidad",
-    },
-    {
-      title: "Estado",
-      dataIndex: "estado",
-      key: "estado",
-      render: (estado) => {
-        let color = "";
-        if (estado === "Confirmada") color = "green";
-        else if (estado === "Pendiente") color = "blue";
-        else if (estado === "Cancelada") color = "red";
-        return <Tag color={color}>{estado}</Tag>;
-      },
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <div className="space-x-2">
+          <a
+            href={record.scheduling_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:text-blue-700"
+          >
+            Schedule
+          </a>
+        </div>
+      ),
     },
   ];
 
   return (
     <div className="p-8 bg-gray-100 min-h-screen">
       <h1 className="text-2xl font-bold mb-4">Reservaciones</h1>
-      <div className="bg-white p-4 shadow rounded">
+      {/* <div className="bg-white p-4 shadow rounded">
         <Table
           columns={columns}
           dataSource={data}
           pagination={{ pageSize: 10 }}
         />
-      </div>
+      </div> */}
+      {isAuthenticatedWithCalendly ? (
+        <div>
+          <h1>Eventos de Calendly</h1>
+          {/* Aquí puedes mostrar los eventos */}
+          <div className="overflow-auto p-4 bg-white rounded-lg shadow-md">
+            <Table
+              columns={columns}
+              dataSource={listEventTypes}
+              rowKey={(record) => record.uri}
+              pagination={{ pageSize: 5 }}
+              bordered
+              className="ant-table-thead-bg-gray-50"
+            />
+          </div>
+        </div>
+      ) : (
+        <button
+          onClick={() => (window.location.href = `${apiUrl}/oauth/calendly`)}
+          // onClick={() => getAuthentication()}
+        >
+          Conectar con Calendly
+        </button>
+      )}
     </div>
   );
 };
